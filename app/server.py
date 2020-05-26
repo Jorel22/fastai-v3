cfaab79f0855544b03cc548438ca6bb89ea627a3
Jorel22 <44505609+Jorel22@users.noreply.github.com>
2020-05-25 20:38:07 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1D-bZwexqS_dKh6h8zM11iAVi83Qya91Z'
+export_file_name = 'trained_model34.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['covid', 'noCovid']
 path = Path(__file__).parent
 
 app = Starlette()
